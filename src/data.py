69729e09981babf3b47e7e8f6447b649564e7076
Danilo Pereira <developer@danilo.info>
2021-11-05 10:32:18 -0300

    Include library

---
+++ src/data.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+from typing import Iterable, TypeVar, Dict, List, Callable, Hashable
+
+T = TypeVar('T')
+K = TypeVar('K', bound=Hashable, covariant=True)
+
+
+def group_by(collection: Iterable[T], key: Callable[[T], K]) -> Dict[K, List[T]]:
+    res: Dict[K, List[T]] = defaultdict(list)
+    for value in collection:
+        res[key(value)].append(value)
+    return res
