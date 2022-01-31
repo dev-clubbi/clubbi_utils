d3386dd6d6b701f5a149c8057b7a955df91a7ea9
Baquit <arthur@clubbi.com.br>
2022-01-31 15:41:52 -0300

    correct typo

--- clubbi_utils/clubbi/slack_integration.py
+++ clubbi_utils/clubbi/slack_integration.py
@@ -6,10 +6,10 @@ from clubbi_utils.clubbi.async_messenger import AsyncSenderStr
 class SlackMessenger(AsyncSenderStr):
     def __init__(self, token: str, channel_name: str):
         self._client = AsyncWebClient(token=token)
-        self._chanel_name = channel_name
+        self._channel_name = channel_name
 
     async def send(self, message: str) -> None:
         await self._client.chat_postMessage(
-            channel=self._chanel_name,
+            channel=self._channel_name,
             text=message,
         )
