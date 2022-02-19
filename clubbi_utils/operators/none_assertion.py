655d15e6364978a2d90996ef4a844afd9f08fa7d
Giuliano Oliveira <giuliano.programador@gmail.com>
2022-02-19 11:33:20 -0300

    feat: none_assertion

---
+++ clubbi_utils/operators/none_assertion.py
@@ -0,0 +1,11 @@
+from typing import TypeVar, Optional
+
+class ValueIsNoneError(ValueError):
+    pass
+
+T = TypeVar("T")
+
+def none_assertion(v: Optional[T]) -> T:
+    if v is None:
+        raise ValueIsNoneError()
+    return v
