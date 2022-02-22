750f14aebf8ca6a02a36b2faee1dfe8ac66f51c3
Paulo Macedo <95712059+paulomaced@users.noreply.github.com>
2022-02-22 15:04:03 -0300

    decorator to log time in functions (#36)
    * decorator to log time in functions
    
    * rename decorator to with_timeit
    
    * rename decorator to with_timeit

---
+++ clubbi_utils/common/timeit.py
@@ -0,0 +1,30 @@
+import time
+from inspect import iscoroutinefunction
+
+from clubbi_utils.logger import logger
+
+
+def with_timeit(f):
+    meta = dict(
+        qualname=f.__qualname__,
+        __module__=f.__module__,
+    )
+
+    def wrapper(*args, **kwargs):
+        begin = time.time()
+        r = f(*args, **kwargs)
+        end = time.time()
+        logger.info(dict(name=f.__name__, duration=end - begin, meta=meta))
+        return r
+
+    async def async_wrapper(*args, **kwargs):
+        begin = time.time()
+        r = await f(*args, **kwargs)
+        end = time.time()
+        logger.info(dict(name=f.__name__, duration=end - begin, meta=meta))
+        return r
+
+    if iscoroutinefunction(f):
+        return async_wrapper
+    else:
+        return wrapper
