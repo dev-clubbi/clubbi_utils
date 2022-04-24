88745e4f51e68d85bfcdf2415b86ae69af658517
Giuliano Oliveira <giuliano.programador@gmail.com>
2022-04-24 12:35:19 -0300

    feat(batched_gather): return_exceptions

--- tests/common/test_batched_gather.py
+++ tests/common/test_batched_gather.py
@@ -1,11 +1,14 @@
-from typing import List
+from typing import List, Optional, Union
 from clubbi_utils.common.batched_gather import batched_gather
 import asyncio
 import time
 from unittest import IsolatedAsyncioTestCase
+from itertools import zip_longest
 
 
-async def sleep_and_return(delay: float) -> float:
+async def sleep_and_return(delay: Optional[float]) -> float:
+    if delay is None:
+        raise RuntimeError()
     await asyncio.sleep(delay)
     return delay
 
@@ -14,9 +17,17 @@ async def run_and_join_results(collection: List[float], number_of_workesrs: int)
     return sum([b async for b in batched_gather(sleep_and_return, collection, number_of_workesrs)], [])
 
 
+async def run_and_join_results_with_exception(
+    collection: List[Optional[float]],
+    number_of_workesrs: int,
+) -> List[Union[float, Exception]]:
+    return sum([b async for b in batched_gather(sleep_and_return, collection, number_of_workesrs, True)], [])
+
+
 class TestBatchedGather(IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         self._collection = [0.125] * 6
+        self._err_collection = [0.1 if i % 2 else None for i in range(5)]
         return super().setUp()
 
     async def test_batched_gather(self):
@@ -33,3 +44,11 @@ class TestBatchedGather(IsolatedAsyncioTestCase):
         time_took_full = time.time() - start
 
         self.assertTrue(time_took_half >= time_took_full)
+
+    async def test_batched_gather_return_exception(self):
+        output = await run_and_join_results_with_exception(self._err_collection, len(self._err_collection) // 2)
+        for f, o in zip_longest(self._err_collection, output):
+            if f is not None:
+                self.assertEqual(f, o)
+            else:
+                self.assertTrue(isinstance(o, RuntimeError))
