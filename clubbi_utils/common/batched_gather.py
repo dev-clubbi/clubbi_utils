7c0ec022d5751d76ceafd29dcd3d07addf82a25c
Giuliano Oliveira <giuliano.programador@gmail.com>
2022-04-25 15:00:15 -0300

    Feat/batched_gather_return_exceptions (#68)
    * fix: missing __init__.py
    
    * feat(batched_gather): return_exceptions
    
    * refact: typo

--- clubbi_utils/common/batched_gather.py
+++ clubbi_utils/common/batched_gather.py
@@ -1,15 +1,38 @@
 import asyncio
 from clubbi_utils.common.iter_as_chunks import iter_as_chunks
-from typing import AsyncGenerator, Awaitable, Callable, Iterable, List, TypeVar
+from typing import AsyncIterator, Awaitable, Callable, Iterable, List, TypeVar, overload, Literal, Union
 
 OUTPUT = TypeVar("OUTPUT")
 INPUT = TypeVar("INPUT")
 
 
-async def batched_gather(
+@overload
+def batched_gather(
     f: Callable[[INPUT], Awaitable[OUTPUT]],
     collection: Iterable[INPUT],
     number_of_workers: int,
-) -> AsyncGenerator[List[OUTPUT], None]:
+    return_exceptions: Literal[True],
+) -> AsyncIterator[List[Union[Exception, OUTPUT]]]:
+    pass
+
+
+@overload
+def batched_gather(
+    f: Callable[[INPUT], Awaitable[OUTPUT]],
+    collection: Iterable[INPUT],
+    number_of_workers: int,
+    return_exceptions: bool = False,
+) -> AsyncIterator[List[OUTPUT]]:
+    pass
+
+
+async def batched_gather(  # type: ignore[misc]
+    f: Callable[[INPUT], Awaitable[OUTPUT]],
+    collection: Iterable[INPUT],
+    number_of_workers: int,
+    return_exceptions: bool = False,
+) -> AsyncIterator[List[Union[Exception, OUTPUT]]]:
     for batch in iter_as_chunks(collection, number_of_workers):
-        yield list(await asyncio.gather(*(f(input_) for input_ in batch)))
+        coros = (f(input_) for input_ in batch)
+        chunk = list(await asyncio.gather(*coros, return_exceptions=return_exceptions))
+        yield chunk
