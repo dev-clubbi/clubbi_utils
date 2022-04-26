3c067a2f8a43b00b5b0e09eec2b7271c7be8d926
Giuliano Oliveira <giuliano.programador@gmail.com>
2022-04-26 15:23:37 -0300

    Feat: json_logging (#72)
    * feat: logging env
    
    * feat: json_logger

--- clubbi_utils/logging.py
+++ clubbi_utils/logging.py
@@ -1,10 +1,37 @@
+import enum
+from pydantic import BaseSettings
 import logging
 
+
+class LogLevelEnum(enum.Enum):
+    fatal = "FATAL"
+    error = "ERROR"
+    warning = "WARNING"
+    info = "INFO"
+    debug = "DEBUG"
+    notset = "NOTSET"
+
+    def to_python_log_level(self) -> int:
+        return {
+            LogLevelEnum.fatal: logging.FATAL,
+            LogLevelEnum.error: logging.ERROR,
+            LogLevelEnum.warning: logging.WARNING,
+            LogLevelEnum.info: logging.INFO,
+            LogLevelEnum.debug: logging.DEBUG,
+            LogLevelEnum.notset: logging.NOTSET,
+        }.get(self, logging.INFO)
+
+
+class LoggingSettings(BaseSettings):
+    log_level: LogLevelEnum = LogLevelEnum.info
+
+
+log_level = LoggingSettings().log_level.to_python_log_level()
 if len(logging.getLogger().handlers) > 0:
     # The Lambda environment pre-configures a handler logging to stderr. If a handler is already configured,
     # `.basicConfig` does not execute. Thus we set the level directly.
-    logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger().setLevel(log_level)
 else:
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=log_level)
 
 logger = logging.getLogger()
