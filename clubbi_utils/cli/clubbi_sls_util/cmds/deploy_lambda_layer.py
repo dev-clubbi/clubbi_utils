0599298dc926e37c63fe06e50e3acd8f55f8a73c
Giuliano Oliveira <giuliano.programador@gmail.com>
2022-06-15 12:21:51 -0300

    feat: added clubbi_sls_utils_deploy_lambda_layer (#102)
    * feat: added clubbi_sls_utils_deploy_lambda_layer
    
    * docs: added info about aws creds
    
    * docs: h4 instead of h5
    
    * Update clubbi_utils/cli/clubbi_sls_util/cmds/deploy_lambda_layer.py
    
    Co-authored-by: Mary Christensen <38894894+maryfsc@users.noreply.github.com>
    
    * refact: OscommandError
    
    * refact: using asyncio
    
    Co-authored-by: Mary Christensen <38894894+maryfsc@users.noreply.github.com>

---
+++ clubbi_utils/cli/clubbi_sls_util/cmds/deploy_lambda_layer.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+
+from argparse import ArgumentParser
+from typing import Dict, Optional
+import hashlib
+from pathlib import Path
+import asyncio
+from ..utils import run_os_cmd
+
+LAMBDA_LAYER_REVISION_KEY = "LambdaLayerRevision"
+LOCK_PATH = Path("Pipfile.lock")
+
+
+async def get_remote_revision(stage: str) -> Optional[str]:
+    cmd = f"npx sls info -s {stage} -c lambda_layer.yml --verbose"
+    out_lines = (await run_os_cmd(cmd, echo=False)).splitlines()
+
+    revision_line = next(
+        (line for line in out_lines if LAMBDA_LAYER_REVISION_KEY in line),
+        None,
+    )
+
+    if revision_line is None:
+        return None
+
+    return revision_line.split(": ")[-1]
+
+
+def get_local_revision() -> str:
+    return hashlib.md5(LOCK_PATH.read_bytes()).hexdigest()
+
+
+async def deploy(stage: str, local_revision: str) -> None:
+    cmd = f"npx sls deploy -c lambda_layer.yml -s {stage}"
+    await run_os_cmd(cmd, env={"LOCK_HASH": local_revision})
+    print(f"Deployed in {stage}!")
+
+
+async def deploy_lambda_layer(stage: str, force: bool) -> None:
+    local_revision = get_local_revision()
+    print(f"{local_revision=}")
+
+    if force:
+        print("Forcing deploy")
+        return await deploy(stage, local_revision)
+
+    remote_revision = await get_remote_revision(stage)
+    print(f"{remote_revision=}")
+
+    if local_revision == remote_revision:
+        print("Versions are the same, let's do nothing!")
+        return
+    print("Versions are different, let's deploy!")
+    await deploy(stage, local_revision)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("stage", type=str, help="Layer stage")
+    parser.add_argument("--force", action="store_true")
+    args = parser.parse_args()
+    asyncio.run(deploy_lambda_layer(args.stage, args.force))
