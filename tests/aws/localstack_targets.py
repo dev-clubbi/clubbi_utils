7efea58d2d90ed87864f4d06eae7149823d5b98b
Giuliano Oliveira <giuliano.programador@gmail.com>
2022-06-15 11:00:42 -0300

    fix: incorrect typing

--- tests/aws/localstack_targets.py
+++ tests/aws/localstack_targets.py
@@ -1,14 +1,17 @@
 from aiobotocore import AioSession
-from aiobotocore.client import AioBaseClient
+from aiobotocore.session import ClientCreatorContext
 
 
 class LocalStack:
-    SNS_TARGET = 'http://localhost:4566'
-    SES_TARGET = 'http://localhost:4566'
+    SNS_TARGET = "http://localhost:4566"
+    SES_TARGET = "http://localhost:4566"
 
 
-def create_test_client(session: AioSession, service_name: str) -> AioBaseClient:
-    return session.create_client(service_name, region_name='us-east-1',
-                                 aws_access_key_id='foo', aws_secret_access_key='bar',
-                                 endpoint_url=LocalStack.SES_TARGET
-                                 )
+def create_test_client(session: AioSession, service_name: str) -> ClientCreatorContext:
+    return session.create_client(
+        service_name,
+        region_name="us-east-1",
+        aws_access_key_id="foo",
+        aws_secret_access_key="bar",
+        endpoint_url=LocalStack.SES_TARGET,
+    )
