6716baeb5caee5aab611b73f3a2fb2179e4a10f7
newtonjose <josephnewton001@gmail.com>
2022-06-22 13:15:18 -0300

    feat: add path parameters to aws http event
    fix imports
    create request context fields

--- clubbi_utils/aws/aws_http_event.py
+++ clubbi_utils/aws/aws_http_event.py
@@ -1,9 +1,28 @@
-class AwsHttpEvent(TypedDict):
-    headers: Dict[str, str]
-    queryStringParameters: Optional[Dict[str, str]]
+from typing import Optional, TypedDict, Dict
+
+__all__ = [
+    "AwsHttpEvent",
+    "AwsHttpResponse"
+]
 
 
-class AwsHttpResponse(TypedDict):
-    statusCode: int
+class RequestContext(TypedDict, total=False):
+    authentication: Dict[str, str]
+    authorizer: Dict[str, str]
+
+
+# Aws payload format version 2.0 (simplified)
+# REF.: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html
+class AwsHttpEvent(TypedDict, total=False):
     body: str
     headers: Dict[str, str]
+    queryStringParameters: Optional[Dict[str, str]]
+    pathParameters: Dict[str, str]
+    requestContext: RequestContext
+
+
+class AwsHttpResponse(TypedDict, total=False):
+    statusCode: int
+    headers: Dict[str, str]
+    body: Optional[str]
+
