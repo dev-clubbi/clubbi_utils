d9a5ad4cca9de105acbcfb0f4067acd89dc67129
Josenilton S. J <josephnewton001@gmail.com>
2022-06-22 16:04:45 -0300

    feat: add path parameters to aws http event (#109)
    * feat: add path parameters to aws http event
    
    fix imports
    create request context fields
    
    * feat: bump 3.4.1
    
    * feat: isolate test
    
    * feat: test get path params
    
    * chore: correct bump
    
    * refactor: better name
    
    * chore: open with read text
    
    Co-authored-by: Giuliano Oliveira <giuliano.programador@gmail.com>
    
    * fix: identation
    
    Co-authored-by: Giuliano Oliveira <giuliano.programador@gmail.com>

--- clubbi_utils/aws/aws_http_event.py
+++ clubbi_utils/aws/aws_http_event.py
@@ -1,9 +1,24 @@
-class AwsHttpEvent(TypedDict):
-    headers: Dict[str, str]
-    queryStringParameters: Optional[Dict[str, str]]
+from typing import Optional, TypedDict, Dict, List
+
+__all__ = [
+    "AwsHttpEvent",
+    "AwsHttpResponse"
+]
+
+# Aws payload format version 2.0 (simplified)
+# REF.: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html
 
 
-class AwsHttpResponse(TypedDict):
-    statusCode: int
+class AwsHttpEvent(TypedDict, total=False):
     body: str
     headers: Dict[str, str]
+    queryStringParameters: Dict[str, str]
+    pathParameters: Dict[str, str]
+
+
+class AwsHttpResponse(TypedDict, total=False):
+    statusCode: int
+    body: Optional[str]
+    headers: Optional[Dict[str, str]]
+    cookies: Optional[List[str]]
+    isBase64Encoded: Optional[bool]
---
+++ tests/aws/test_aws_http_event.py
@@ -0,0 +1,30 @@
+import json
+from pathlib import Path
+from unittest import IsolatedAsyncioTestCase
+
+from clubbi_utils.aws.aws_http_event import AwsHttpEvent
+
+PARENT_DIR = Path(__file__).parent
+
+
+class TestAwsHttpEvent(IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        data = json.loads(PARENT_DIR.joinpath("local_mocks/static/aws_http_payload_v2.json").read_text())
+
+        self.payload = data
+        self.event: AwsHttpEvent = data
+
+    def test_parse_aws_http_event(self):
+        assert self.event == AwsHttpEvent(
+            body="Hello from Lambda",
+            headers={"header1": "value1", "header2": "value1,value2"},
+            queryStringParameters={"parameter1": "value1,value2", "parameter2": "value"},
+            pathParameters={"parameter1": "value1"}
+        )
+
+    def test_get_path_parameters(self):
+        path_params = self.event.get("pathParameters")
+
+        assert path_params
+        assert path_params.get("parameter1") == self.payload['pathParameters']['parameter1']
