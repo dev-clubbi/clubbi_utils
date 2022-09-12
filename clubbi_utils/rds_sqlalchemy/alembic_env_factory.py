4d341b2ea62aeb971c1b1acef88ad1d97a188a0f
Danilo Pereira <developer@danilo.info>
2022-09-12 17:27:15 -0300

    alembic environment easy to point to RDS.

---
+++ clubbi_utils/rds_sqlalchemy/alembic_env_factory.py
@@ -0,0 +1,111 @@
+import asyncio
+import logging
+import sys
+from logging.config import fileConfig
+from os import getenv
+from typing import cast, Any, Optional
+
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+from alembic.config import Config
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import MetaData
+
+from clubbi_utils.logging import logger
+from clubbi_utils.rds_sqlalchemy.get_rds_config import load_rds_sqlalchemy_config
+
+try:
+    config = context.config
+    assert isinstance(config.config_file_name, str)
+    fileConfig(config.config_file_name)
+except AttributeError:
+    logging.exception(dict(message='run using alembic CLI'))
+    exit(1)
+
+
+async def get_sqlalchemy_url(config: Config) -> Optional[str]:
+    my_env = getenv('ALEMBIC_RDS_ENVIRONMENT', None)
+
+    if my_env:
+        driver = config.get_main_option('sql_driver')
+        rds_data = config.get_section(f'rds.{my_env}')
+        assert rds_data
+        pg_config = await load_rds_sqlalchemy_config(
+            rds_instance_id=rds_data['rds_datainstance_id'],
+            database_name=rds_data['database_name'],
+            user_name=rds_data['user_name'],
+            password_secret_id=rds_data['password_secret_id'],
+            driver=driver,
+        )
+        if sys.stdin.isatty():
+            in_ = input(f"""\nAre changing [\033[1m{my_env}\033[0m] database?\nconfirm typing yes:""")
+            if 'yes' != in_:
+                logger.warn("skipping because you did not confirm it with `yes`...")
+                return None
+        url = str(pg_config.to_sqlalchemy_uri())
+    else:
+        url = getenv('SQLALCHEMY_URI', config.get_main_option("sqlalchemy.url") or '')
+
+    return url
+
+
+def run_alembic_env(target_metadata: MetaData) -> None:
+    async def run_migrations_offline():
+        """Run migrations in 'offline' mode.
+
+        This configures the context with just a URL
+        and not an Engine, though an Engine is acceptable
+        here as well.  By skipping the Engine creation
+        we don't even need a DBAPI to be available.
+
+        Calls to context.execute() here emit the given string to the
+        script output.
+
+        """
+
+        url = await get_sqlalchemy_url(config)
+        if url is None:
+            return
+        config.set_main_option('sqlalchemy.url', url)
+        context.configure(
+            url=url,
+            target_metadata=target_metadata,
+            literal_binds=True,
+            dialect_opts={
+                "paramstyle": "named"
+            },
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+    async def run_migrations_online():
+        """Run migrations in 'online' mode.
+
+        In this scenario we need to create an Engine
+        and associate a connection with the context.
+
+        """
+        url = await get_sqlalchemy_url()
+        if url is None:
+            return
+
+        engine = create_async_engine(url)
+
+        def do_migrations(connection_):
+            options = cast(dict[str, Any], config.get_section(config.config_ini_section))
+            context.configure(connection=connection_, target_metadata=target_metadata, **options)
+
+            with context.begin_transaction():
+                context.run_migrations()
+
+        async with engine.connect() as connection:
+            await connection.run_sync(do_migrations)
+
+    if context.is_offline_mode():
+        asyncio.run(run_migrations_offline())
+    else:
+        asyncio.run(run_migrations_online())
---
+++ clubbi_utils/rds_sqlalchemy/get_rds_config.py
@@ -0,0 +1,36 @@
+from asyncio import gather
+
+from aiobotocore.client import AioBaseClient
+from clubbi_utils.aws.decorator import with_aws_client
+from clubbi_utils.sqlalchemy.connector import SqlAlchemyConfig
+
+
+@with_aws_client("secretsmanager")
+@with_aws_client("rds")
+async def load_rds_sqlalchemy_config(
+        rds: AioBaseClient,
+        secretsmanager: AioBaseClient,
+        rds_instance_id: str,
+        database_name: str,
+        user_name: str,
+        password_secret_id: str,
+        driver_name: str,
+) -> SqlAlchemyConfig:
+    database_describe, password_data = await gather(
+        rds.describe_db_instances(DBInstanceIdentifier=rds_instance_id),
+        secretsmanager.get_secret_value(
+            password_secret_id,
+        ),
+    )
+
+    password = password_data["SecretString"]
+    endpoint = database_describe["DBInstances"][0]["Endpoint"]
+
+    return SqlAlchemyConfig(
+        database=database_name,
+        user=user_name,
+        password=password,
+        host=endpoint["Address"],
+        port=endpoint["Port"],
+        driver_name=driver_name,
+    )
