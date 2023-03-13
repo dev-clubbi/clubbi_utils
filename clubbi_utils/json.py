bbb08cbf8f8cdb2d70c7ef25fd12c51298f7a701
Abel Rocha Espinosa <abel.roes@gmail.com>
2023-03-13 15:41:57 -0300

    :sparkles: Add OPT_NON_STR_KEYS for std python json.dumps (#178)
    * :sparkles: Add OPT_NON_STR_KEYS for std python json.dumps
    
    * Add import
    
    * fix: added non_str_keys to default dumps as well
    
    * :bookmark: Bump to v4.0.0
    
    ---------
    
    Co-authored-by: Giuliano Oliveira <giuliano.programador@gmail.com>

--- clubbi_utils/json.py
+++ clubbi_utils/json.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 try:
     import orjson
-    from orjson import dumps as _dumps, loads as _loads, OPT_INDENT_2, OPT_SORT_KEYS
+    from orjson import dumps as _dumps, loads as _loads, OPT_INDENT_2, OPT_SORT_KEYS, OPT_NON_STR_KEYS
     from decimal import Decimal
 
     def _serialize_default(obj: Any) -> Any:
@@ -17,13 +17,15 @@ try:
         raise TypeError
 
     def dumps(
-        data: Any, /, pretty: bool = False, sort_keys: bool = False, default: Callable[[Any], Any] = _serialize_default
+        data: Any, /, pretty: bool = False, sort_keys: bool = False, non_str_keys: bool = True, default: Callable[[Any], Any] = _serialize_default
     ) -> str:
         option = 0
         if pretty:
             option |= OPT_INDENT_2
         if sort_keys:
             option |= OPT_SORT_KEYS
+        if non_str_keys:
+            option |= OPT_NON_STR_KEYS
 
         return _dumps(data, option=option, default=default).decode()
 
@@ -41,7 +43,7 @@ except ImportError:
         return pydantic_encoder(obj)
 
     def dumps(
-        data: Any, /, pretty: bool = False, sort_keys: bool = False, default: Callable[[Any], Any] = _default
+        data: Any, /, pretty: bool = False, sort_keys: bool = False, non_str_keys: bool = True, default: Callable[[Any], Any] = _default
     ) -> str:
         return default_dumps(data, default=default, sort_keys=sort_keys, indent=2 if pretty else None)
 
