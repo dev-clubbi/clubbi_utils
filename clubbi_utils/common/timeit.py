0aedd762cb0a7026733a228bd46b0e5b7d5b6335
Giuliano Macedo <giuliano.programador@gmail.com>
2023-11-06 15:37:01 -0300

    feat: using jlogger in with_timeit

--- clubbi_utils/common/timeit.py
+++ clubbi_utils/common/timeit.py
@@ -1,27 +1,30 @@
 import time
 from inspect import iscoroutinefunction
+from typing import Callable
+from clubbi_utils.json_logging import jlogger
 
-from clubbi_utils.logging import logger
 
-
-def with_timeit(f):
+def _log(f: Callable, begin: float, end: float) -> None:
     meta = dict(
         qualname=f.__qualname__,
-        __module__=f.__module__,
+        module=f.__module__,
     )
+    jlogger.info(f.__name__, "time_elapsed", duration=end - begin, meta=meta)
 
+
+def with_timeit(f: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         begin = time.time()
         r = f(*args, **kwargs)
         end = time.time()
-        logger.info(dict(name=f.__name__, duration=end - begin, meta=meta))
+        _log(f, begin, end)
         return r
 
     async def async_wrapper(*args, **kwargs):
         begin = time.time()
         r = await f(*args, **kwargs)
         end = time.time()
-        logger.info(dict(name=f.__name__, duration=end - begin, meta=meta))
+        _log(f, begin, end)
         return r
 
     if iscoroutinefunction(f):
