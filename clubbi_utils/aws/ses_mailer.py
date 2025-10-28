122a2cb06b9d0245e90d8317e90fa8a33c59b2fa
Juliana <julianadepaula001@gmail.com>
2025-10-28 11:45:49 -0300

    test actions

--- clubbi_utils/aws/ses_mailer.py
+++ clubbi_utils/aws/ses_mailer.py
@@ -24,6 +24,7 @@ class SesMailer:
         logger.info(response["MessageId"])
 
     def _build_email_data(self, email: Email) -> dict:
+        print("actions")
         data = dict(
             Destination={"ToAddresses": email.recipients, "BccAddresses": [CLUBBI_CONTROL_EMAIL]},
             Message={
